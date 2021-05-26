5e49748fc48af73ce9cf0e7b178c60f9cbb34415
Tanner Daines <dainestanner7@gmail.com>
2021-05-26 16:14:18 -0600

    Created a function that calculates the correct lead or lag to match up the manual and detector data together and joins the manual and detector data into one data table.

--- R/data_functions.R
+++ R/data_functions.R
@@ -30,17 +30,60 @@ clean_data <- function(raw_data){
     )
 }
 
-#' Adjust the timebins
+#' Adjust the time bins and combine detector and manual counts into one data frame
 #' 
 #' @param cleaned_data
-#' @return A dataframe with the time bins corrected
+#' @return A data frame with the time bins corrected
+#'
+
+
+
 adjust_timebins <- function(raw_detector_data, raw_manual_data){
+  # figure out how to do lead/lag adjustment, and then apply it to the spreadsheets
+  rmse_lag <- function(detector, manual, nlags){
+    n <- 0
+    manual_lag <- manual
+    if(nlags > 0){
+      while(n < nlags){
+        manual_lag <- lag(manual_lag) 
+        n <- n + 1
+      }
+    } else if(nlags < 0){
+      #if nlags is negative, use lead
+      while(n < -1 * nlags){
+        manual_lag <- lead(manual_lag) 
+        n <- n + 1
+      }
+    }
+    sqrt(mean((detector - manual)^2, na.rm = TRUE))
+  }  
+    
   
-  # TODO: Adjust time stamps, join together
-  cleaned_data
   
+  
+  # do a left_join here to combine the spreadsheets together, but only after doing the lead/lag
+  
+ 
+  lag_value <- vector()
+  for(i in c(-3:3)){
+   lag_value[i+4] <- rmse_lag(raw_detector_data$det_eq_1,raw_manual_data$man_eq_1,i)  
+  }
+  
+  correct_lag <- which.min(lag_value) - 4
+  
+  raw_manual_data %>% 
+    mutate(
+      start_new = lag(start_time, n = correct_lag), # figure out what to do if correct_lag is negative (if statement of if correct_lag is positive or negative)
+      end_new = lag(end_time, n = correct_lag) # figure out what the joining is actually doing when shifting the data (does it move the data up or down)
+    ) %>% 
+    left_join(raw_detector_data, by = c("start_new" = "start_time", "end_new" = "end_time"))
 }
 
+
+
+  # TODO: Adjust time stamps, join together
+#  cleaned_data
+  
 #' Nest the data
 #' 
 #' @param cleaned_data

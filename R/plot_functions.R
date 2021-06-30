bf5f11e3974bc547a1da6426be475c1340279b08
Tanner Daines <dainestanner7@gmail.com>
2021-06-30 16:25:45 -0600

    Created function to make different queue length estimates and then plot these estimates against each other.

--- R/plot_functions.R
+++ R/plot_functions.R
@@ -24,10 +24,43 @@ plot_optim_default_rmse <- function(group_k){
     scale_color_viridis_c()
 }
 
-#ggplot(model_data, aes(x = density, y = optim_k)) + 
-#  geom_point() + 
-#  stat_poly_eq(formula = log(y) ~ x, aes(label = paste(..eq.label..,..rr.label..,sep="~~~")), parse = TRUE) +
-#  geom_smooth(method="lm",se=FALSE)
-#  #geom_smooth(method = "glm", formula = y ~ x, se = FALSE,
-#  #            method.args = list(family = gaussian(link = 'log')))
+old_plot <- function(group_k){
+  ggplot(model_data, aes(x = density, y = optim_k, color = ramp, fill = factor(ramp))) + 
+    geom_point() + 
+    stat_poly_eq(formula = log(y) ~ x, aes(label = paste(..eq.label..,..rr.label..,sep="~~~")), parse = TRUE) +
+    #geom_smooth(method="lm",se=FALSE)
+    geom_smooth(method = "glm", formula = y ~ x, se = FALSE,
+                method.args = list(family = gaussian(link = 'log')))
+}
 
+
+plot_predicted_queues <- function(linearmodels, model_data){
+  pdata <- model_data %>%
+    ungroup() %>%
+    mutate(
+      estimated_k = predict(linearmodels[['Ramp Control']]), # change which model?
+      # heuristics
+      heur_k = case_when(
+        flow > 1200 ~ 0.6,
+        density > 25 ~ 0.8,
+        TRUE ~ 0.22
+      )
+    ) %>%
+    select(ramp, day,  data, optim_k, queue_at_k, estimated_k, heur_k) %>%
+    mutate(
+      timestamp = map(data, function(x) x$start_time),
+      queue_model = map2(estimated_k, data, kalman_queue),
+      queue_observed = map(data, function(x) x$man_q_len),
+      queue_horowitz = map2(0.22, data, kalman_queue),
+      queue_heuristic = map2(heur_k, data, kalman_queue)
+    ) %>%
+    select(-data) %>%
+    rename(queue_optimal = queue_at_k) %>%
+    unnest(cols = c(timestamp, contains("queue"))) %>%
+    pivot_longer(cols = contains("queue"), names_to = "Series", values_to = "Queue")
+  
+  ggplot(pdata %>% filter(day %in% c(9, 12)), aes(x = timestamp, y = Queue, color = Series)) + 
+    geom_line() +
+    facet_grid(ramp ~ day, scales = "free_x") + theme_bw()
+  
+}
